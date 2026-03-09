6ff9c4825197ddab2ce60218f08aed060fe2fd14
mhpark <mhpark@i-screammedia.com>
2026-03-09 17:29:51 +0900

    33강까지 완료

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppHeader
+    v-bind:appTitle="message"
+    v-on:change="changeMessage">
+  </AppHeader>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import AppHeader from './components/AppHeader.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      //'AppHeader': AppHeader
+      AppHeader
+    },
+    data() {
+      return {
+        message: '앱 헤더 컴포넌트'
+      }
+    },
+    methods: {
+      changeMessage() {
+        this.message = "변경됨"
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
