0b592f98ae25fb8076438da1d7392c4cdf47d15b
mhpark <mhpark@i-screammedia.com>
2026-03-12 17:20:23 +0900

    37강까지 완강

---
+++ vue3-form/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <form action="" @submit.prevent="submitForm">
+    <div>
+      <label for="userId">ID:</label>
+      <input id="userId" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">PW:</label>
+      <input id="password" type="text" v-model="password">
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+  import axios from 'axios';
+  import { ref } from 'vue';
+
+  export default {
+    setup() {
+      var username = ref('');
+      var password = ref('');
+
+      var submitForm = () => {
+        axios.post('https://jsonplaceholder.typicode.com/users/', {
+          username: username.value,
+          password: password.value
+        }).then(response => {
+          console.log(response);
+        });
+      }
+
+      return { username, password, submitForm }
+    }
+
+    
+    // data() {
+    //   return {
+    //     username: '',
+    //     password: '',
+    //   }
+    // },
+    // methods: {
+    //   submitForm() {
+    //     const data = {
+    //       username: this.username,
+    //       password: this.password,
+    //     }
+
+    //     axios.post('https://jsonplaceholder.typicode.com/users/', data)
+    //     .then(response => {
+    //       console.log(response)
+    //     });
+    //   }
+    // }
+  }
+</script>
+
+<style scoped>
+
+</style>
